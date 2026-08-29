1c6468988bbcecbf9fbbea07e1d6d03b31766423
Mohd Areeb Fakhar <70427381+Areeb7867@users.noreply.github.com>
2026-08-29 19:14:00 +0530

    Add city cost staging model

---
+++ analytics/models/staging/stg_city_costs.sql
@@ -0,0 +1,15 @@
+with source_data as (
+    select * from {{ ref('city_costs') }}
+),
+
+cleaned as (
+    select
+        lower(trim(city)) as city,
+        lower(trim(category)) as category,
+        cast(monthly_estimated_cost as numeric) as monthly_estimated_cost,
+        cast(updated_at as date) as updated_at
+    from source_data
+)
+
+select * from cleaned
+
